b6fe1568af251c6e433d5ff70db00e958a6c632f
hal7df@gmail.com <hal7df@gmail.com@210dcb69-d170-9f69-680e-9f744e0cea6d>
2013-06-07 12:25:29 +0000

    JK! This should be it.

---
+++ trunk/2013/PlatePIDAccelHandler.h
@@ -0,0 +1,16 @@
+#include "WPILib.h"
+
+class PlatePIDAccelHandler: public PIDOutput , public PIDSource
+{
+    public:
+        PlatePIDAccelHandler(Victor* m_Motor1, Victor* m_Motor2, AnalogChannel* m_PlatePot, ADXL345_SPI *m_plateAccel, double m_PlateAccelFiltered);
+        void PIDWrite(float output);
+        double PIDGet();
+    protected:
+    private:
+        ADXL345_SPI *m_Accel;
+        Victor* m_PlateMotor1; //Plate Motor
+        Victor* m_PlateMotor2;
+        AnalogChannel* m_PlatePot; //Plate Potentiometer
+        double m_PlateAccelFiltered;
+};
