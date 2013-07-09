2f40bcaadb98702f9bfa05dbf2c57f4ec14e6694
hal7df <hal7df@210dcb69-d170-9f69-680e-9f744e0cea6d>
2013-07-09 21:46:49 +0000

    NEW: tutorial/Defines.h
    Defines.h
    =========
    Added Defines.h to the tutorial/ directory and made it application-independent (Robot-specific values have been removed).

---
+++ trunk/tutorial/Defines.h
@@ -0,0 +1,33 @@
+//Buttons
+
+#define BUTTON_A 1
+#define BUTTON_B 2
+#define BUTTON_X 3
+#define BUTTON_Y 4
+#define BUTTON_LB 5
+#define BUTTON_RB 6
+#define BUTTON_BACK 7
+#define BUTTON_START 8
+#define BUTTON_L3 9
+#define BUTTON_R3 10
+
+//Axes
+
+#define LEFT_X 1
+#define LEFT_Y 2
+#define TRIGGERS 3
+#define RIGHT_X 4
+#define RIGHT_Y 5
+
+#define PracticeBot
+//#define CompetitionBot
+
+
+#ifdef PracticeBot
+  //Place practice bot values here
+#endif
+
+#ifdef CompetitionBot
+  //Place competition bot values here
+#endif
+
